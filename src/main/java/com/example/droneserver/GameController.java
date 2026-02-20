d7319637a3b1c177ed85d699448fbb5014773681
valejasi <vjasidakis@gmail.com>
2026-02-20 12:32:26 -0300

    cambios en gamecontroller y position

--- src/main/java/com/example/droneserver/GameController.java
+++ src/main/java/com/example/droneserver/GameController.java
@@ -67,6 +67,4 @@ public class GameController {
         return sala.getPosiciones();
     }
     
-
-
 }
--- src/main/java/com/example/droneserver/Position.java
+++ src/main/java/com/example/droneserver/Position.java
@@ -4,9 +4,13 @@ public class Position {
     public float x;
     public float y;
     public float z;
+    public float qx;
+    public float qy;
+    public float qz;
+    public float qw;
 
     @Override
     public String toString() {
-        return "x=" + x + ", y=" + y + ", z=" + z;
+        return "x=" + x + ", y=" + y + ", z=" + z + ", qx=" + qx + ", qy=" + qy + ", qz=" + qz + ", qw" + qw;
     }
 }
