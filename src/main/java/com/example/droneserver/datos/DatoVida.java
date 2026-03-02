e75ecde0d839e408f59b249660571133ac599ffa
valejasi <vjasidakis@gmail.com>
2026-03-02 15:59:38 -0300

    ultimo cambio

--- src/main/java/com/example/droneserver/datos/DatoVida.java
+++ src/main/java/com/example/droneserver/datos/DatoVida.java
@@ -2,13 +2,11 @@ package com.example.droneserver.datos;
 
 public class DatoVida {
     public int objId;   
-    public String tipo;    
     public int vida;
 
     public DatoVida() {}
-    public DatoVida(int objId, String tipo, int vida) {
+    public DatoVida(int objId, int vida) {
         this.objId = objId;
-        this.tipo = tipo;
         this.vida = vida;
     }
 }
