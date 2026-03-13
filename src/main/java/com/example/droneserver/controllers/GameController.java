4cdbc34068771e0733ff490e8fd2156ec8c1dc40
ceciliaD <cedallagiovanna@gmail.com>
2026-03-13 00:33:50 -0300

    sfdgf

--- src/main/java/com/example/droneserver/controllers/GameController.java
+++ src/main/java/com/example/droneserver/controllers/GameController.java
@@ -247,7 +247,7 @@ public class GameController {
 			Position portaPos = enemigo.getPortaPosicion();
 			if (portaPos != null && enemigo.getPortaVida() > 0) {
 				float dist = distancia(p.x, p.y, p.z, portaPos.x, portaPos.y, portaPos.z);
-				if (dist <= 9.0f) {
+				if (dist <= 20.0f) {
 					enemigo.aplicarDanio(enemigo.getObjIdPorta(), p.danio);
 					p.activo = false;
 					continue;
